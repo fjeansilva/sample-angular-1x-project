e223f9cd98200671c3d14ab4142f9b17226dff8f
Jean Silva <jeancarlos.silva@me.com>
2020-04-21 16:14:32 -0300

    Simple Controller

---
+++ controllers/main.controller.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('MainController', MainController);
+
+  function MainController($http) {
+
+    var vm = this;
+
+    var onUserSuccess = function (result) {
+      vm.person = result.data;
+    };
+
+    var onError = function (err) {
+      vm.error = 'Cannot get the user information';
+    };
+
+    vm.message = 'Welcome to the Angular';
+
+    $http.get('http://api.github.com/users/fjeansilva')
+      .then(onUserSuccess, onError);
+    
+  };  
+
+}());
